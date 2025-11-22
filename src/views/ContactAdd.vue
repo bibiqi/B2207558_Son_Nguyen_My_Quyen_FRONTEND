cc863f79a904cacb7adb5cd8a8c9a743b2a52aa2
MyQuyen <myquyen031@gmail.com>
2025-11-22 23:13:29 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page">
+    <h4>Thêm Liên hệ Mới</h4>
+    <ContactForm
+      :contact="contact"
+      @submit:contact="addContact"
+    />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      // Khởi tạo dữ liệu rỗng cho form thêm mới
+      contact: {
+          name: "",
+          email: "",
+          address: "",
+          phone: "",
+          favorite: false
+      },
+      message: "",
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        // Gọi hàm create từ ContactService
+        await ContactService.create(data);
+        alert('Liên hệ được thêm thành công.');
+        this.$router.push({ name: "contactbook" });
+      } catch (error) {
+        console.log(error);
+        this.message = "Đã có lỗi xảy ra khi thêm liên hệ.";
+      }
+    },
+  },
+};
+</script>
